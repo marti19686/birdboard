f7838badbbea7a3c96aada969ce9f2645e6b628b
marti19686 <marti19686@gmail.com>
2020-04-09 23:25:09 +0200

    Styles project page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,24 +2,18 @@
 
 @section('content')
     <header class="flex items-center mb-3">
-        <div class="flex justify-between items-center w-full">
+        <div class="flex justify-between items-end w-full">
             <h2 class="text-gray-600 text-sm font-normal">My Projects</h2>
 
-            <a href="/projects/create" class="button">New Project</a>
+            <a href="/projects/create" class="button">Add Project</a>
         </div>
     </header>
 
     <main class="lg:flex lg:flex-wrap -mx-3 py-4">
         @forelse($projects as $project)
-        <div class="lg:w-1/3 px-3 pb-6">
-            <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
-                <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-400 pl-4">
-                    <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
-                </h3>
-
-                <div class="text-gray-600">{{ Str::limit($project->description, 100) }}</div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                @include('projects.card')
             </div>
-        </div>
         @empty
             <div>No projects yet.</div>
         @endforelse
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,8 +2,41 @@
 
 @section('content')
 
-    <h1>{{ $project->title }}</h1>
-    <div>{{ $project->description }}</div>
-    <a href="/projects">Go Back</a>
+    <header class="flex items-center mb-3">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-gray-600 text-sm font-normal">
+                <a href="/projects" class="text-gray-600 text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+            </p>
+
+            <a href="/projects/create" class="button">Add Project</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-6">
+                <div class="mb-8">
+                    <h2 class="text-gray-600 text-lg font-normal mb-3">Tasks</h2>
+
+                    {{-- tasks --}}
+                    <div class="card mb-3">Lorem ipsum</div>
+                    <div class="card mb-3">Lorem ipsum</div>
+                    <div class="card mb-3">Lorem ipsum</div>
+                    <div class="card mb-3">Lorem ipsum</div>
+                </div>
+
+                <div>
+                    <h2 class="text-gray-600 text-lg font-normal">General Notes</h2>
+
+                    {{-- general notes --}}
+                    <textarea class="card w-full" style="min-height: 200px">Lorem ipsum</textarea>
+                </div>
+            </div>
+
+            <div class="lg:w-1/4 px-3">
+                @include('projects.card')
+            </div>
+        </div>
+    </main>
 
 @endsection
